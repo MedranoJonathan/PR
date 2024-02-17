472d2dc273a25521106b04d2acfdf2136025196e
MedranoJonathan <157251566+MedranoJonathan@users.noreply.github.com>
2024-02-16 21:42:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function enviar(){
+    var nombre= document.getElementById("nombre").value;
+    var apellido= document.getElementById("apellido").value;
+    var gemail= document.getElementById("mail").value;
+    alert(nombre + " " + apellido + ", ¡tu mensaje ha sido enviado correctamente! Pronto recibiras noticias en tu email con los horarios disponibles y los precios");
+    
+}
